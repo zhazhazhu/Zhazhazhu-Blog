c9b4d47d214d6f4a23df43922a6f124a0b58d4aa
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-04 23:55:16 +0800

    feat: add routers Layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +8,17 @@ export default defineConfig({
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
+      '/@': path.join(__dirname, 'src'),
       'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
       'balm-ui-css': 'balm-ui/dist/balm-ui.css'
     }
-  }
+  },
+  css: {
+    /* CSS 预处理器 */
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "src/assets/scss/style.scss";'
+      }
+    }
+  },
 })
