93cddb648f8fd141971559ce2e2b3079d5398cf2
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-14 02:10:10 +0800

    feat: 添加config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 const hostname = 'http://110.42.131.59'
 
+
 const ports = {
   blogs: 3000
 }
@@ -8,4 +9,4 @@ const hosts = {
   blogs: `${hostname}:${ports.blogs}`
 }
 
-window.projectConfig = {hosts}
+window.projectConfig = { hosts }
