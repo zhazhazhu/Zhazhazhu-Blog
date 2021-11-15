a83f6c4aa0e4b2853a55bc202e729b2feca0983d
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-16 00:36:04 +0800

    feat: 完成首页

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,52 +1,51 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
+import dayjs from 'dayjs';
 </script>
 
 <template>
   <el-container>
-    <el-aside style="max-width: 90px">
+    <el-aside>
       <el-menu
-          default-active="home"
-          class="el-menu-vertical"
-          router
-          @open="handleOpen"
-          @close="handleClose"
+        default-active="home"
+        class="el-menu-vertical"
+        router
+        @open="handleOpen"
+        @close="handleClose"
       >
         <div class="logo">
           <ui-icon :size="40">trip_origin</ui-icon>
         </div>
         <el-menu-item index="home">
-          <ui-icon :size="18">house_siding</ui-icon>
-          首 页
+          <ui-icon :size="18">house_siding</ui-icon>首 页
         </el-menu-item>
         <el-menu-item index="/file">
-          <ui-icon :size="18">folder_open</ui-icon>
-          归 档
+          <ui-icon :size="18">folder_open</ui-icon>归 档
         </el-menu-item>
         <el-menu-item index="3">
-          <ui-icon :size="18">crop_original</ui-icon>
-          相 册
+          <ui-icon :size="18">crop_original</ui-icon>相 册
         </el-menu-item>
         <el-menu-item index="3">
-          <ui-icon :size="18">opacity</ui-icon>
-          收 藏
+          <ui-icon :size="18">opacity</ui-icon>收 藏
         </el-menu-item>
         <el-menu-item index="3">
-          <ui-icon :size="18">ios_share</ui-icon>
-          关 于
+          <ui-icon :size="18">ios_share</ui-icon>关 于
         </el-menu-item>
         <el-menu-item index="3">
-          <ui-icon :size="18">search</ui-icon>
-          搜 索
+          <ui-icon :size="18">search</ui-icon>搜 索
         </el-menu-item>
       </el-menu>
+      <el-backtop>
+        <ui-icon>arrow_circle_up</ui-icon>
+      </el-backtop>
     </el-aside>
-    <el-main>
-      <router-view/>
-    </el-main>
+    <el-container>
+      <router-view />
+      <div class="footer-info">Q渣渣猪 © {{dayjs().format('YYYY')}}</div>
+    </el-container>
   </el-container>
 </template>
 
 <style lang="scss">
-@import "src/assets/scss/home";
+@import "src/assets/scss/layout.scss";
 </style>
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,28 +1,75 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import { Fold } from '@element-plus/icons'
-import { asyncImage } from '../util/image';
+import dayjs from 'dayjs'
+import { getBlogList } from '../api/blogs';
+import { searchListModel } from '../api/types/blogsModel';
+import { blogListModel } from './types/homeModel';
 
-const imageUrls = ref<string[]>([])
+const initBlogList = () => ref<blogListModel[]>([])
+
+const blogList = initBlogList()
+
+const searchData = ref<searchListModel>({
+  pageIndex: 1, //当前页
+  pageSize: 10, //展示多少条数据
+  keyWord: "", //搜索
+  sortField: "",
+  sortType: "",
+  totalCount: 0, //总共多少条
+})
+
+async function getBlog() {
+  const { code, data, pages, total } = await getBlogList(searchData.value)
+  if (code === 1) {
+    searchData.value.pageIndex = pages || 1
+    searchData.value.totalCount = total || 1
+    blogList.value = data
+  }
+}
+
+const icon1 = {
+  on: 'favorite',
+  off: 'favorite_border'
+}
+const icon2 = {
+  on: 'bookmark',
+  off: 'bookmark_border'
+}
 
 onMounted(() => {
-  asyncImage().then((res) => {
-    imageUrls.value = res
-  })
+  getBlog()
 })
 
 </script>
 
 <template>
   <div class="header-img">
-    <el-image fit="cover" style="width: 100%;height: 100vh" src="src/assets/image/home1.jpg"></el-image>
+    <el-image fit="cover" style="width: 100%;height: 100vh" src="src/assets/image/Sunflower1.jpg"></el-image>
   </div>
   <div class="aside-button">
     <ui-icon-button icon="menu_open" outlined></ui-icon-button>
   </div>
+  <div v-for="blog in blogList" :key="blog.id">
+    <el-card class="box-card" shadow="never">
+      <template #header>
+        <div class="card-header">
+          <h1>{{ blog.title }}</h1>
+          <div class="created-time">
+            <ui-icon size="16">date_range</ui-icon>
+            {{ blog.createdAt }}
+          </div>
+        </div>
+      </template>
+      <div>{{ blog.content }}</div>
+    </el-card>
+  </div>
+  <div class="footer">
+    <el-pagination layout="prev, pager, next" :total="searchData.totalCount"></el-pagination>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@import "./style/home.scss";
 .aside-button {
   position: absolute;
   top: 20px;
