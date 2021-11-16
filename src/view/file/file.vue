828f718587f4da04d342e33a1555aa6eaa652bcb
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-16 22:04:05 +0800

    feat: 增加归档页面

--- src/view/file/file.vue
+++ src/view/file/file.vue
@@ -1,11 +1,58 @@
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import dayjs from 'dayjs'
+import { getBlogList } from '../../api/blogs';
+import { searchListModel } from '../../api/types/blogsModel';
+import { blogListModel } from './types/fileModel';
+
+const initBlogList = () => ref<blogListModel[]>([])
+
+const blogList = initBlogList()
+
+const searchData = ref<searchListModel>({
+  pageIndex: 1, //当前页
+  pageSize: 20, //展示多少条数据
+  keyWord: "", //搜索
+  sortField: "",
+  sortType: "",
+  totalCount: 0, //总共多少条
+})
+
+async function getBlog() {
+  const { code, data, pages, total } = await getBlogList(searchData.value)
+  if (code === 1) {
+    searchData.value.pageIndex = pages || 1
+    searchData.value.totalCount = total || 1
+    blogList.value = data
+  }
+}
+
+onMounted(() => {
+  getBlog()
+})
 
 </script>
 
 <template>
-<div>111</div>
+  <div class="p-4">
+    <header>
+      <h2>归档</h2>
+    </header>
+    <main>
+      <div class="content">
+        <div v-for="blog in blogList" :key="blog.id" class="list-content">
+          <div class="icon"></div>
+          <div class="time">{{ dayjs(blog.createdAt).format('MM/DD') }}</div>
+          <el-button type="text" class="title">{{ blog.title }}</el-button>
+        </div>
+      </div>
+    </main>
+    <footer>
+    <el-pagination layout="prev, pager, next" :total="searchData.totalCount"></el-pagination>
+  </footer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+@import "./style/file.scss";
 </style>
---
+++ src/view/file/types/fileModel.ts
@@ -0,0 +1,7 @@
+export interface blogListModel {
+  id: string
+  title: string;
+  content: string;
+  author: string;
+  createdAt: string;
+}
