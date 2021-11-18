7cbcdfc4e3d8e74e0f84d9c769b01cdef48b51e8
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-18 17:28:06 +0800

    feat: 增加获取照片接口

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -2,9 +2,14 @@ import { defHttp } from '../util/http/index';
 import { searchListModel } from './types/blogsModel';
 
 enum API {
-  blogListModel = 'api/blog/list'
+  blogListModel = 'api/blog/list',
+  GetUploadPhoto = 'api/Form/GetUploadPhoto'
 }
 
 export function getBlogList(search: searchListModel) {
   return defHttp.post(API.blogListModel, search)
+}
+
+export function getUploadPhoto(id?: string) {
+  return defHttp.get(API.GetUploadPhoto, id)
 }
