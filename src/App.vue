eb20fb66c862474384265987b2c669297e120e26
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-18 17:28:54 +0800

    feat: 缓存路由

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 </script>
 
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive" />
+    </keep-alive>
+    <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive" />
+  </router-view>
 </template>
 
 <style lang="scss">
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,35 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'TabBar',
     component: () => import('/@/layout/index.vue'),
-    redirect:'/home',
+    redirect: '/home',
     children: [
       {
         path: 'home',
         name: 'Home',
-        component: () => import('../view/home/home.vue')
+        component: () => import('../view/home/home.vue'),
+        meta: {
+          keepAlive: true
+        }
       },
       {
         path: '/file',
         name: 'File',
-        component: () => import('../view/file/file.vue')
+        component: () => import('../view/file/file.vue'),
+        meta: {
+          keepAlive: true
+        }
       },
       {
         path: '/photo',
         name: 'Photo',
-        component: () => import('../view/photo/index.vue')
+        component: () => import('../view/photo/index.vue'),
+        meta: {
+          keepAlive: true
+        }
       }
     ]
   },
