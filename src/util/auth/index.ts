1e0ff83879fc982e620fdbb2592273c6eea5567e
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-20 00:56:25 +0800

    feat: 添加获取local token数据

--- src/util/auth/index.ts
+++ src/util/auth/index.ts
@@ -1,7 +1,13 @@
+import { useUserStore } from '/@/store/modules/user';
 enum Local {
   TOKEN = 'VUE_QZZHU_TOKEN'
 }
 
 export function setLocalToken(token: string) {
   window.localStorage.setItem(Local.TOKEN, token)
+}
+
+export function getLocalToken(){
+  const userStore = useUserStore()
+  return window.localStorage.getItem(Local.TOKEN) || userStore.getToken
 }
