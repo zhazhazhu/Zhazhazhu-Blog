0b9b7dab1060ee0e80919dc0977b62a8d21376f5
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-20 15:22:11 +0800

    feat: 增加注册用户信息类型

--- src/view/login/types/index.ts
+++ src/view/login/types/index.ts
@@ -2,3 +2,11 @@ export interface userModel {
   userName: string;
   passWord: string
 }
+
+export interface userDataModel {
+  userName: string;
+  passWord: string;
+  email: string;
+  avatar: string;
+  phoneNumber: string;
+}
