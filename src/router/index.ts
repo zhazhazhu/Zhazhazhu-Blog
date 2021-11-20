ddea244c913d85a6d6ca732adb18938a8bc75899
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-20 15:19:11 +0800

    feat: 增加注册路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import blogRoutes from './model/blog';
 import loginRoutes from './model/login';
 
-const routes: RouteRecordRaw[] = [blogRoutes, loginRoutes]
+const routes: RouteRecordRaw[] = [blogRoutes, ...loginRoutes]
 
 const router = createRouter({
   history: createWebHistory(),
--- src/router/model/login.ts
+++ src/router/model/login.ts
@@ -1,12 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { RouteRecordRaw } from 'vue-router'
 
-const loginRoutes: RouteRecordRaw ={
-  path: '/login',
-  name: 'Login',
-  component: () => import('/@/view/login/index.vue'),
-  meta: {
-    keepAlive: true
+const loginRoutes: RouteRecordRaw[] = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('/@/view/login/index.vue'),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('/@/view/register/index.vue'),
+    meta: {
+      keepAlive: true
+    }
   }
-}
+]
 
 export default loginRoutes
