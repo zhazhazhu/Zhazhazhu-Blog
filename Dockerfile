92302f7b9616130edff260ebde5a30a7e795e0d1
zhazhazhu <fangyuan.leslie@qq.com>
2021-11-21 13:53:41 +0800

    feat: 添加部署文件

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM nginx
+
+LABEL name="zhazhazhu-blog"
+
+LABEL version="0.0.1"
+
+#工作目录
+WORKDIR /
+
+COPY ./dist /usr/share/nginx/html
+
+COPY ./zhazhazhu.conf /etc/nginx/conf.d
+
+EXPOSE 80
---
+++ script.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+WORK_PATH='/web/blogs/Zhazhazhu-Blog'
+cd $WORK_PATH
+echo 'remote old code'
+git remote -v
+echo 'pull new code'
+git fetch origin
+echo 'merge new code'
+git merge origin/master
+echo 'build code'
+npm run build
+echo 'start build'
+docker build -t zhazhazhu-blog:1.0 .
+echo 'stop old docker'
+docker stop zhazhazhu-blog-old
+echo 'delete old docker ps'
+docker rm zhazhazhu-blog-old
+echo 'delete old docker images'
+docker rmi zhazhazhu-blog-old
+echo 'run new docker'
+docker container run -p 80:80 --name zhazhazhu-blog-old -d zhazhazhu-blog:1.0
+echo "deploy end"
